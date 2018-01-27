bb800e162ff4bca59e473e5b4db304383d7cfabe
catpalm <35228211+catpalm@users.noreply.github.com>
2018-01-27 13:24:02 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -4,7 +4,7 @@ import sys, random
 
 assert sys.version_info >= (3, 4), "This script requires at least Python 3.4"
 
-# variables
+
 
 guess_range = 20
 tries = 0
@@ -15,17 +15,17 @@ person = input('Enter Name:')
 print('Excellent', person, ', now I know your name!')
 print('Now try to find the hidden number within 10 tries')
 
-# generate a random integer between 1 and 20 (inclusive) and store it in the variable [number]
+
 number = random.randint(1, guess_range)
 
-# ask the user for a response and store it in the variable [guess]
+
 
 
 try:
 
 
 
-# a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
+
 	while tries < 10:
 		guess = input()
 		guess = int(guess)
